5a759971214a7e270b454f8da8e71f73a6bb0495
David Matthams <dmatthams@gmail.com>
2021-04-14 11:02:23 +0100

    APP ID

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ echo $api_url
 token=$(curl "${api_url}/oauth/access_token?email=${INPUT_EMAIL}"&api_key=${INPUT_API_KEY}"" | jq -r ".access_token")
 echo $token
 
-result=$(curl -XPOST -H "Authorization: Bearer ${token}" "${api_url}/app/manage/webroot?app_id=${APP_ID}"&server_id=${INPUT_SERVER_ID}"&webroot=${INPUT_WEBROOT}"" | jq -r ".status" )
+result=$(curl -XPOST -H "Authorization: Bearer ${token}" "${api_url}/app/manage/webroot?app_id=${INPUT_APP_ID}"&server_id=${INPUT_SERVER_ID}"&webroot=${INPUT_WEBROOT}"" | jq -r ".status" )
 echo "::set-output name=success::$result"
