191b97fa3eb2f54bee8e38b6a76b34185c13126a
David Matthams <dmatthams@gmail.com>
2021-04-14 11:05:45 +0100

    More quote cleaning

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,10 @@
 api_url="https://api.cloudways.com/api/v1"
 echo $api_url
 
-token=$(curl "${api_url}/oauth/access_token?email=${INPUT_EMAIL}"&api_key=${INPUT_API_KEY}"" | jq -r ".access_token")
+token=$(curl "${api_url}/oauth/access_token?email=${INPUT_EMAIL}&api_key=${INPUT_API_KEY}" | jq -r ".access_token")
 echo $token
 
 result=$(curl -XPOST -H "Authorization: Bearer ${token}" "${api_url}/app/manage/webroot?app_id=${INPUT_APP_ID}&server_id=${INPUT_SERVER_ID}&webroot=${INPUT_WEBROOT}" | jq -r ".status" )
+echo $result
+
 echo "::set-output name=success::$result"
