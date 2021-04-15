f28e76c7276c47ee8701d76c19cec9ecc02afd2b
David Matthams <dmatthams@gmail.com>
2021-04-15 09:56:19 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-
 api_url="https://api.cloudways.com/api/v1"
 echo $api_url
 
 token=$(curl "${api_url}/oauth/access_token?email=${INPUT_EMAIL}&api_key=${INPUT_API_KEY}" | jq -r ".access_token")
 echo $token
 if [[ $token = "null" ]]; then
-  echo "${RED}Could not obtain access token, check account email and api key.${NC}"
+  echo "Could not obtain access token, check account email and api key."
   exit 1
 fi
 
 result=$(curl -XPOST -H "Authorization: Bearer ${token}" "${api_url}/app/manage/webroot?app_id=${INPUT_APP_ID}&server_id=${INPUT_SERVER_ID}&webroot=${INPUT_WEBROOT}" | jq -r ".status" )
 echo $result
 if [[ ! $result = "true" ]]; then
-  echo "${RED}Could not change webroot, check App & Site ID's are correct.${NC}"
+  echo "Could not change webroot, check App & Site ID's are correct."
   exit 1
 fi
 
-echo "${GREEN}Webroot successfully updated to [${INPUT_WEBROOT}]${NC}"
+echo "Webroot successfully updated to [${INPUT_WEBROOT}]"
 
 echo "::set-output name=success::$result"
