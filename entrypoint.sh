59672d0b2e063cf41d6245c7ee1a7c70c39339d7
David Matthams <dmatthams@gmail.com>
2021-04-15 09:32:29 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@ api_url="https://api.cloudways.com/api/v1"
 echo $api_url
 
 token=$(curl "${api_url}/oauth/access_token?email=${INPUT_EMAIL}&api_key=${INPUT_API_KEY}" | jq -r ".access_token")
-if [ -z "$token" ]; then
+if [ ! "$token" ]; then
   echo "Could not obtain access token, check account email and api key."
   exit 1
 fi
 
 result=$(curl -XPOST -H "Authorization: Bearer ${token}" "${api_url}/app/manage/webroot?app_id=${INPUT_APP_ID}&server_id=${INPUT_SERVER_ID}&webroot=${INPUT_WEBROOT}" | jq -r ".status" )
-if [ -z "$result" ]; then
+if [ ! "$result" ]; then
   echo "Could not change webroot, check App & Site ID's are correct."
   exit 1
 fi
